0e77a90ccc7aa2ab78e37a1a7c3f5bce7ed8afd5
Gabor <renyhart.gabor@gmail.com>
2020-10-21 04:23:03 +0200

    Tanárnál a pdf órarend órái ugyanakkor végződtek, megoldva (nem volt jó az indexelés)

--- program/application/views/tanar/orarend_pdf.php
+++ program/application/views/tanar/orarend_pdf.php
@@ -17,17 +17,17 @@
 						switch($ora['hanyadik_ora'])
 						{
 							//$a jelenti a 0. órát, $b az 1.-t stb.
-							case 0:$a[$nap]=$osztaly."<BR>".$tantargy."<BR>".$terem;break;
-							case 1:$b[$nap]=$osztaly."<BR>".$tantargy."<BR>".$terem;break;
-							case 2:$c[$nap]=$osztaly."<BR>".$tantargy."<BR>".$terem;break;
-							case 3:$d[$nap]=$osztaly."<BR>".$tantargy."<BR>".$terem;break;
-							case 4:$e[$nap]=$osztaly."<BR>".$tantargy."<BR>".$terem;break;
-							case 5:$f[$nap]=$osztaly."<BR>".$tantargy."<BR>".$terem;break;
-							case 6:$g[$nap]=$osztaly."<BR>".$tantargy."<BR>".$terem;break;
-							case 7:$h[$nap]=$osztaly."<BR>".$tantargy."<BR>".$terem;break;
-							case 8:$j[$nap]=$osztaly."<BR>".$tantargy."<BR>".$terem;break;
-							case 9:$k[$nap]=$osztaly."<BR>".$tantargy."<BR>".$terem;break;
-							case 10:$l[$nap]=$osztaly."<BR>".$tantargy."<BR>".$terem;break;
+							case 0:$a[$nap]=$osztaly."<BR>".$tantargy."<BR>Terem: ".$terem;break;
+							case 1:$b[$nap]=$osztaly."<BR>".$tantargy."<BR>Terem: ".$terem;break;
+							case 2:$c[$nap]=$osztaly."<BR>".$tantargy."<BR>Terem: ".$terem;break;
+							case 3:$d[$nap]=$osztaly."<BR>".$tantargy."<BR>Terem: ".$terem;break;
+							case 4:$e[$nap]=$osztaly."<BR>".$tantargy."<BR>Terem: ".$terem;break;
+							case 5:$f[$nap]=$osztaly."<BR>".$tantargy."<BR>Terem: ".$terem;break;
+							case 6:$g[$nap]=$osztaly."<BR>".$tantargy."<BR>Terem: ".$terem;break;
+							case 7:$h[$nap]=$osztaly."<BR>".$tantargy."<BR>Terem: ".$terem;break;
+							case 8:$j[$nap]=$osztaly."<BR>".$tantargy."<BR>Terem: ".$terem;break;
+							case 9:$k[$nap]=$osztaly."<BR>".$tantargy."<BR>Terem: ".$terem;break;
+							case 10:$l[$nap]=$osztaly."<BR>".$tantargy."<BR>Terem: ".$terem;break;
 						}
 					}
 				?>
@@ -51,70 +51,70 @@
 		        </tr>
 		        <tr>
 		        	<td>1<BR></td>
-					<td><?=$csengrend[1]['kezdes']?> - <?=$csengrend[0]['vege']?></td>
+					<td><?=$csengrend[1]['kezdes']?> - <?=$csengrend[1]['vege']?></td>
 		        	<?php for($i=1;$i<6;$i++):?>
 					<?php if(isset($b[$i])):?><td><?=$b[$i]?></td><?php else:?><td><br><br></td><?php endif;?>
 		        	<?php endfor;?>
 		        </tr>
 		        <tr>
 		        	<td>2<BR></td>
-					<td><?=$csengrend[2]['kezdes']?> - <?=$csengrend[0]['vege']?></td>
+					<td><?=$csengrend[2]['kezdes']?> - <?=$csengrend[2]['vege']?></td>
 		        	<?php for($i=1;$i<6;$i++):?>
 					<?php if(isset($c[$i])):?><td><?=$c[$i]?></td><?php else:?><td><br><br></td><?php endif;?>
 		        	<?php endfor;?>
 		        </tr>
 		        <tr>
 		        	<td>3<BR></td>
-					<td><?=$csengrend[3]['kezdes']?> - <?=$csengrend[0]['vege']?></td>
+					<td><?=$csengrend[3]['kezdes']?> - <?=$csengrend[3]['vege']?></td>
 		        	<?php for($i=1;$i<6;$i++):?>
 					<?php if(isset($d[$i])):?><td><?=$d[$i]?></td><?php else:?><td><br><br></td><?php endif;?>
 		        	<?php endfor;?>
 		        </tr>
 		        <tr>
 		        	<td>4<BR></td>
-					<td><?=$csengrend[4]['kezdes']?> - <?=$csengrend[0]['vege']?></td>
+					<td><?=$csengrend[4]['kezdes']?> - <?=$csengrend[4]['vege']?></td>
 		        	<?php for($i=1;$i<6;$i++):?>
 					<?php if(isset($e[$i])):?><td><?=$e[$i]?></td><?php else:?><td><br><br></td><?php endif;?>
 		        	<?php endfor;?>
 		        </tr>
 		        <tr>
 		        	<td>5<BR></td>
-					<td><?=$csengrend[5]['kezdes']?> - <?=$csengrend[0]['vege']?></td>
+					<td><?=$csengrend[5]['kezdes']?> - <?=$csengrend[5]['vege']?></td>
 		        	<?php for($i=1;$i<6;$i++):?>
 					<?php if(isset($f[$i])):?><td><?=$f[$i]?></td><?php else:?><td><br><br></td><?php endif;?>
 		        	<?php endfor;?>
 		        </tr>
 		        <tr>
 		        	<td>6<BR></td>
-					<td><?=$csengrend[6]['kezdes']?> - <?=$csengrend[0]['vege']?></td>
+					<td><?=$csengrend[6]['kezdes']?> - <?=$csengrend[6]['vege']?></td>
 		        	<?php for($i=1;$i<6;$i++):?>
 					<?php if(isset($g[$i])):?><td><?=$g[$i]?></td><?php else:?><td><br><br></td><?php endif;?>
 		        	<?php endfor;?>
 		        </tr>
 		        <tr>
 		        	<td>7<BR></td>
-					<td><?=$csengrend[7]['kezdes']?> - <?=$csengrend[0]['vege']?></td>
+					<td><?=$csengrend[7]['kezdes']?> - <?=$csengrend[7]['vege']?></td>
 		        	<?php for($i=1;$i<6;$i++):?>
 					<?php if(isset($h[$i])):?><td><?=$h[$i]?></td><?php else:?><td><br><br></td><?php endif;?>
 		        	<?php endfor;?>
 		        </tr>
 		        <tr>
 		        	<td>8<BR></td>
-					<td><?=$csengrend[8]['kezdes']?> - <?=$csengrend[0]['vege']?></td>
+					<td><?=$csengrend[8]['kezdes']?> - <?=$csengrend[8]['vege']?></td>
 		        	<?php for($i=1;$i<6;$i++):?>
 					<?php if(isset($j[$i])):?><td><?=$j[$i]?></td><?php else:?><td><br><br></td><?php endif;?>
 		        	<?php endfor;?>
 		        </tr>
 		        <tr>
 		        	<td>9<BR></td>
-					<td><?=$csengrend[9]['kezdes']?> - <?=$csengrend[0]['vege']?></td>
+					<td><?=$csengrend[9]['kezdes']?> - <?=$csengrend[9]['vege']?></td>
 		        	<?php for($i=1;$i<6;$i++):?>
 					<?php if(isset($k[$i])):?><td><?=$k[$i]?></td><?php else:?><td><br><br></td><?php endif;?>
 		        	<?php endfor;?>
 		        </tr>
 		        <tr>
 		        	<td>10<BR></td>
-					<td><?=$csengrend[10]['kezdes']?> - <?=$csengrend[0]['vege']?></td>
+					<td><?=$csengrend[10]['kezdes']?> - <?=$csengrend[10]['vege']?></td>
 		        	<?php for($i=1;$i<6;$i++):?>
 					<?php if(isset($l[$i])):?><td><?=$l[$i]?></td><?php else:?><td><br><br></td><?php endif;?>
 		        	<?php endfor;?>
